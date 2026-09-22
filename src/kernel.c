bfb15581910d42593c2021ec1dd2c84d1fb3d631
Mulky Siraj Firizqi <mulkysiraj0412@gmail.com>
2026-09-22 12:19:45 +0700

    feat: Implementasi GDT dan transisi 32-bit Protected Mode

--- src/kernel.c
+++ src/kernel.c
@@ -4,7 +4,8 @@
 #include "header/kernel-entrypoint.h"
 
 void kernel_setup(void) {
-   uint32_t a;
+    load_gdt(&_gdt_gdtr);
+    uint32_t a;
     uint32_t volatile b = 0x0000BABE;
    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
     while (true) b += 1;
